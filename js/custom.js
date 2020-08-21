03d788885ab556ca7098ece04b382b866a6b41be
Rohit Singh <rohits@geekyants.com>
2020-08-21 11:56:47 +0530

    updated og image and few ui fix

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,10 @@
 window.onload = () => {
-	document.querySelector(".csb").style.display = "block";
 	console.log(window.location.toString().includes("example"));
 	if (window.location.toString().includes("example")) {
 		document.querySelector(".onPageNav").style.display = "none";
+	}
+	console.log("why?" + window.location.toString().includes("why"));
+	if (window.location.toString().includes("why")) {
+		document.querySelector(".postHeader").style.display = "none";
 	}
 };
