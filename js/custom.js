67fee19e4f44bb33a09c88d2f951c0fa9e531041
MD-REHMAN <rehman.2468@gmail.com>
2021-05-26 20:23:23 +0530

    added auto redirect

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,14 @@
+window.location = 'http://syncstate.geekyants.com/';
 window.onload = () => {
-	if (window.location.toString().includes("example")) {
-		document.querySelector(".onPageNav").style.display = "none";
+	if (window.location.toString().includes('example')) {
+		document.querySelector('.onPageNav').style.display = 'none';
 	}
-	var script = document.createElement("script");
-	var script1 = document.createElement("script");
+	var script = document.createElement('script');
+	var script1 = document.createElement('script');
 
-	script.src = "https://www.googletagmanager.com/gtag/js?id=UA-177268399-1";
+	script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-177268399-1';
 	var t = document.createTextNode(
-		"window.dataLayer = window.dataLayer || [];function gtag() {	dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-177268399-1');"
+		"window.dataLayer = window.dataLayer || [];function gtag() {	dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-177268399-1');",
 	);
 	script1.appendChild(t);
 	document.head.appendChild(script);
